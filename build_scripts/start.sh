fd1ab1524f80070dd2dd9c561884b12a77365c9a
Dave Compton <davecompton7@gmail.com>
2017-12-01 11:10:06 -0800

    removed conditional on user_exists

--- build_scripts/start.sh
+++ build_scripts/start.sh
@@ -3,11 +3,9 @@
 export USER_NAME=$1
 export USER_KEY=$2
 
-user_exists=$(id -u $USER_NAME > /dev/null 2>&1; echo $?) 
-if [[ "$user_exists" == 0 ]]; then
-    su $USER_NAME -c 'mkdir ~/.ssh'
-    su $USER_NAME -c 'sudo echo $USER_KEY > ~/.ssh/authorized_keys'
-    su $USER_NAME -c 'chmod 600 ~/.ssh/authorized_keys'
-    echo "sshd started"
-    sudo /usr/bin/svscan /services/ 
-fi
+su $USER_NAME -c 'mkdir ~/.ssh'
+su $USER_NAME -c 'sudo echo $USER_KEY > ~/.ssh/authorized_keys'
+su $USER_NAME -c 'chmod 600 ~/.ssh/authorized_keys'
+
+echo "sshd started"
+sudo /usr/bin/svscan /services/ 
