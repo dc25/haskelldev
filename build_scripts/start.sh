90dd234f7809fa96ce12a75b0d91028ccc404ab2
Dave Compton <davecompton7@gmail.com>
2018-05-22 20:49:51 -0700

    added -p to mkdir

--- build_scripts/start.sh
+++ build_scripts/start.sh
@@ -3,7 +3,7 @@
 export USER_NAME=$1
 export USER_KEY=$2
 
-su $USER_NAME -c 'mkdir ~/.ssh'
+su $USER_NAME -c 'mkdir -p ~/.ssh'
 su $USER_NAME -c 'sudo echo $USER_KEY > ~/.ssh/authorized_keys'
 su $USER_NAME -c 'chmod 600 ~/.ssh/authorized_keys'
 
